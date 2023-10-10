6c57c308941eeffa36798ceb22c8b2319fa372fd
Lara RG <laragoblin@outlook.com>
2023-10-10 18:02:19 +0200

    arreglar lo del boton de add que no cierra

--- src/main/java/mythsearcher/mythsearcher/AddCtrl.java
+++ src/main/java/mythsearcher/mythsearcher/AddCtrl.java
@@ -1,4 +1,62 @@
 package mythsearcher.mythsearcher;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
 public class AddCtrl {
+    @FXML
+    private TextField Biotxt;
+
+    @FXML
+    private Button CancelBtn;
+
+    @FXML
+    private ComboBox<?> ClassCmb;
+
+    @FXML
+    private Button CleanBtn;
+
+    @FXML
+    private Button CloseBtn;
+
+    @FXML
+    private ComboBox<?> GenderCmb;
+
+    @FXML
+    private ListView<?> MythList;
+
+    @FXML
+    private TextField Nametxt;
+
+    @FXML
+    private TextField Origintxt;
+
+    @FXML
+    private Button SaveBtn;
+
+    @FXML
+    void OnClickCancel(ActionEvent event) {
+
+    }
+
+    @FXML
+    void OnClickClean(ActionEvent event) {
+
+    }
+
+    @FXML
+    void OnClickClose(ActionEvent event) {
+        Stage stage = (Stage) this.CloseBtn.getScene().getWindow();
+        stage.close();
+    }
+
+    @FXML
+    void OnClickSave(ActionEvent event) {
+
+    }
 }
--- src/main/java/mythsearcher/mythsearcher/AppCtrl.java
+++ src/main/java/mythsearcher/mythsearcher/AppCtrl.java
@@ -2,10 +2,16 @@ package mythsearcher.mythsearcher;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
 
 public class AppCtrl {
     @FXML
@@ -27,8 +33,16 @@ public class AppCtrl {
     private ListView<?> MythosList;
 
     @FXML
-    void OnClickAdd(ActionEvent event) {
+    void OnClickAdd(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("add.fxml"));
+        Parent root = fxmlLoader.load();
+        Scene scene = new Scene(root);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.show();
 
+        Stage stagePrincipal = (Stage) AddBtn.getScene().getWindow();
+        stagePrincipal.close();
     }
 
     @FXML
