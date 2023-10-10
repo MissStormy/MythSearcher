4150f7705e4e6a9e37cbbc1dd05275cc39c794e4
Lara RG <laragoblin@outlook.com>
2023-10-10 17:54:14 +0200

    Main/add frontend finished

--- src/main/java/mythsearcher/mythsearcher/AppCtrl.java
+++ src/main/java/mythsearcher/mythsearcher/AppCtrl.java
@@ -1,14 +1,54 @@
 package mythsearcher.mythsearcher;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.stage.Stage;
 
 public class AppCtrl {
     @FXML
-    private Label welcomeText;
+    private Button AddBtn;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button CloseBtn;
+
+    @FXML
+    private Button EraseBtn;
+
+    @FXML
+    private Button ListBtn;
+
+    @FXML
+    private Button ModBtn;
+
+    @FXML
+    private ListView<?> MythosList;
+
+    @FXML
+    void OnClickAdd(ActionEvent event) {
+
+    }
+
+    @FXML
+    void OnClickClose(ActionEvent event) {
+        Stage stage = (Stage) this.CloseBtn.getScene().getWindow();
+        stage.close();
+    }
+
+    @FXML
+    void OnClickErase(ActionEvent event) {
+
+    }
+
+    @FXML
+    void OnClickList(ActionEvent event) {
+
+    }
+
+    @FXML
+    void OnClickMod(ActionEvent event) {
+
     }
 }
---
+++ src/main/resources/BBDD/Cthulhu.sql
@@ -0,0 +1,20 @@
+-- Esta BBDD va a estar basada en el trabajo de H.P.Lovecraft
+-- Call of Cthulhu
+DROP DATABASE IF EXISTS Cthulhu;
+CREATE DATABASE Cthulhu;
+USE Cthulhu;
+
+-- Esta tabla contiene a todos los mitos conocidos
+CREATE TABLE Mythos (
+Id int unsigned auto_increment primary key,
+Nombre varchar(50),
+Tipo enum('Primigenio', 'Dios Exterior', 'Monstruo'),
+Genero enum('Masculino', 'Femenino', 'Otro'),
+Origen varchar(50),
+Imagen varchar(60)
+);
+INSERT INTO Primigenios VALUES 
+(1, "Cthulhu", "Primigenio","Masculino","Vhoorl",""),
+(2, "Nyarlathotep", "Dios Exterior","Otro","Desconocido",""),
+(3, "Azathoth", "Dios Exterior","Otro","Corte de Azathoth","");
+
